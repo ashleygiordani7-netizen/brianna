af90e38fa557f96e9543aadecd664d1bb5a2275a
ashleygiordani7-netizen <ashleygiordani7@gmail.com>
2025-08-15 23:42:12 +0100

    Update index.js

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,26 +1,10 @@
 
-
 const quoteData = [
     "You are beautiful beyond words",
     "No word has yet to be invented to describe how beautiful you truly are",
-    "You are the reason to get up in the morning and many more reasons to smile throughout the day",
-    "You are truly incredible",
-    "Every time you smile, the world becomes a little brighter",
-    "In a crowd of thousands, my eyes will always search for you first",
-    "You are my favorite thought, my happiest daydream, and my sweetest reality",
-    "The sound of your laughter is my favorite melody",
-    "You make even the simplest moments unforgettable",
-    "You are my safe place and my greatest adventure",
-    "Every moment with you feels like a page from a fairy tale",
-    "Your presence feels like sunshine after days of rain",
-    "The world is more beautiful simply because you exist in it",
-    "You are the best part of my today and the hope of my tomorrow",
-    "Loving you feels like breathing — effortless and essential",
-    "You are the magic I didn’t believe existed until I met you",
-    "Even on the darkest days, your light guides me home",
-    "If I could, I’d write your name in the stars so the universe never forgets you",
-    "Your kindness is as breathtaking as your beauty",
-    "Every heartbeat of mine whispers your name"
+    "You are the funniest person ive ever met",
+    "You are intelligent, brave and courageous every day",
+    "Hope you have a lovely day whatever it may bring"
 ];
 
 function getRandomQuote() {
@@ -51,4 +35,5 @@ const displayMessage = () => {
 
 displayMessage();
 getRandomQuote();
+
 
